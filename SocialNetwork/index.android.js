c95c32a65968b1e8c7ec1427d1a8e153c4f33fcb
adadesions <ada_dev@icloud.com>
2017-04-28 13:00:22 +0700

    Create Application end-point

--- SocialNetwork/index.android.js
+++ SocialNetwork/index.android.js
@@ -6,38 +6,11 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry
 } from 'react-native';
-import getTheme from './native-base-theme/components';
-import material from './native-base-theme/variables/material';
-import {
-  StyleProvider,
-  Container,
-  Header,
-  Content,
-  Footer
-} from 'native-base';
 
-// My Components
-import SynHeader from './components/SynHeader'
-import SynFooter from './components/SynFooter'
-import SynContent from './components/SynContent'
+// App Component
+import App from './components/App'
 
-export default class SocialNetwork extends Component {
-  render() {
-    return (
-      <StyleProvider style={getTheme(material)}>
-        <Container>
-          <SynHeader></SynHeader>
-          <SynContent></SynContent>
-          <SynFooter></SynFooter>
-        </Container>
-      </StyleProvider>
-    );
-  }
-}
 
-AppRegistry.registerComponent('SocialNetwork', () => SocialNetwork);
+AppRegistry.registerComponent('SocialNetwork', () => App);
