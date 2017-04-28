209df2fca5c907c646574dd40176762ec437dfd8
adadesions <ada_dev@icloud.com>
2017-04-28 10:55:12 +0700

    Create new component SynHeader, SynFooter

---
+++ SocialNetwork/components/SynHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {
+  Header
+} from 'native-base'
+
+export default class SynHeader extends Component{
+  render() {
+    return (
+      <Header>
+        
+      </Header>
+    );
+  }
+}
