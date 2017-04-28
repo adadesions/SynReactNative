aa736b54f84c912bde4f0729d781ed1be8f4a38c
Nattawat007 <nattawat.yachum@gmail.com>
2017-04-28 13:57:44 +0700

    add thumbnail by dragonballTeam

--- SocialNetwork/components/SynContent.js
+++ SocialNetwork/components/SynContent.js
@@ -1,45 +1,86 @@
 import React, { Component } from 'react'
-import { Image, ScrollView} from 'react-native'
-import { Container, Content, List, ListItem, Text, Body, Left, Right} from 'native-base';
+import {
+  Image,
+  ScrollView
+} from 'react-native'
+import {
+  Container,
+  Content,
+  List,
+  ListItem,
+  Text,
+  Body,
+  Left,
+  Right,
+  Thumbnail
+} from 'native-base';
+
 import { Col, Row, Grid } from 'react-native-easy-grid';
 
 export default class SynContent extends Component{
   render() {
     return (
-
-
+          <Container>
               <Grid>
-                                      <Row  style={{  height:200 }} >
-                                        <Image source={require('../public/team.jpg')}   style={{ width: '100%', height: '100%'}}/>
-                                      </Row>
-                                      <Row  style={{ width:'100%'}}>
-                                        <List >
-                                              <ListItem >
+                <Row  style={{  height:150 }} >
+                  <Image source={require('../public/team.jpg')}
+                  style={{ width: '100%', height: '100%'}}/>
+                </Row>
+                <Row  style={{ width:'100%',paddingTop: 10}}>
+                  <List >
+                    <ListItem avatar style={{ paddingTop: 10, paddingBottom: 10}}>
+                        <Left>
+                            <Thumbnail source={require('../public/nong.jpg')} />
+                        </Left>
+                        <Right style={{ paddingLeft: 20}}>
+                            <Text>P'Nong</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar style={{ paddingTop: 10, paddingBottom: 10}}>
+                        <Left>
+                            <Thumbnail size={80}  source={require('../public/toy.jpg')} />
+                        </Left>
+                        <Right style={{ paddingLeft: 20}}>
+                            <Text>Toyakung</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar style={{ paddingTop: 10, paddingBottom: 10}}>
+                        <Left>
+                            <Thumbnail size={80}  source={require('../public/nut.jpg')} />
+                        </Left>
+                        <Right style={{ paddingLeft: 20}}>
+                            <Text>Nut</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar style={{ paddingTop: 10, paddingBottom: 10}}>
+                        <Left>
+                            <Thumbnail size={80}  source={require('../public/tao.jpg')} />
+                        </Left>
+                        <Right style={{ paddingLeft: 20}}>
+                            <Text>Tao</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar style={{ paddingTop: 10, paddingBottom: 10}}>
+                        <Left>
+                            <Thumbnail size={80}  source={require('../public/lit.jpg')} />
+                        </Left>
+                        <Right style={{ paddingLeft: 20}}>
+                            <Text>Lit</Text>
+                        </Right>
+                    </ListItem>
+                    <ListItem avatar style={{ paddingTop: 10, paddingBottom: 10}}>
+                        <Left>
+                            <Thumbnail size={80}  source={require('../public/tao.jpg')} />
+                        </Left>
+                        <Right style={{ paddingLeft: 20}}>
+                            <Text>Ball</Text>
+                        </Right>
+                    </ListItem>
 
-                                                  <Text >Tao</Text>
-
-                                              </ListItem>
-                                              <ListItem>
-                                                  <Text>P'Nong</Text>
-                                              </ListItem>
-                                              <ListItem>
-                                                  <Text>Nut</Text>
-                                              </ListItem>
-                                              <ListItem>
-                                                  <Text>Toyakung</Text>
-                                              </ListItem>
-                                              <ListItem>
-                                                  <Text>Lit</Text>
-                                              </ListItem>
-                                              <ListItem>
-                                                  <Text>Ball</Text>
-                                              </ListItem>
-
-                                          </List>
-                                        </Row>
-              </Grid>
-
-
+                </List>
+              </Row>
+            </Grid>
+        </Container>
     );
   }
 }
