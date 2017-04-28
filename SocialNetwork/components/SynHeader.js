dff1beb64ceb8e278c03ac02db0c412f866cae3b
credo2k7 <nawin.juntong@gmail.com>
2017-04-28 14:01:36 +0700

    add header contents

--- SocialNetwork/components/SynHeader.js
+++ SocialNetwork/components/SynHeader.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
 import {
-  Header
+  Header,
+  Left,
+  Body,
+  Title,
+  Button,
+  Icon,
+  Thumbnail,
+  Right,
+  Text
 } from 'native-base'
-
+import {
+  Image
+} from 'react-native'
 export default class SynHeader extends Component{
   render() {
     return (
       <Header>
-
+         <Left>
+            <Button transparent>
+               <Icon name='menu' />
+            </Button>
+         </Left>
+         <Body style={{justifyContent: 'flex-start',flexDirection: 'row'}}>
+            <Image
+               source={require('../public/slri.jpg')}
+               style={{width:'50%', height: '50%'}}
+            />
+            <Text style={{color:'#FFF'}}>Synchrotron</Text>
+         </Body>
+         <Right>
+            <Button transparent>
+               <Icon name='log-out' />
+            </Button>
+         </Right>
       </Header>
     );
   }
