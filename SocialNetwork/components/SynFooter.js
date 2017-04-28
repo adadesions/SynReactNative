40d7a7578d2da445e625d0318f38632ede3e2950
adadesions <ada_dev@icloud.com>
2017-04-28 12:05:41 +0700

    Update SynFooter

--- SocialNetwork/components/SynFooter.js
+++ SocialNetwork/components/SynFooter.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react'
 import {
+  Container,
+  Content,
   Footer,
+  FooterTab,
+  Button,
+  Icon,
+  Badge,
+  Text
 } from 'native-base'
+
+import { Link, Route } from 'react-router-native'
 
 export default class SynFooter extends Component{
   render() {
     return (
       <Footer>
-        
+          <FooterTab>
+                <Button>
+                  <Link to="/">
+                    <Icon name="apps" />
+                  </Link>
+                </Button>
+                <Button>
+                  <Link to="/camera">
+                    <Icon name="camera" />
+                  </Link>
+                </Button>
+              <Button active>
+                  <Icon active name="navigate" />
+              </Button>
+              <Button>
+                  <Icon name="person" />
+              </Button>
+          </FooterTab>
       </Footer>
     );
   }
