7934db642d7983116bcfb4220d9f606e418ccee7
Nattawat007 <nattawat.yachum@gmail.com>
2017-04-28 12:06:06 +0700

    add footer and image by dragonballteam

---
+++ SocialNetwork/components/SynContent.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Image, ScrollView} from 'react-native'
+import { Container, Content, List, ListItem, Text } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+
+export default class SynContent extends Component{
+  render() {
+    return (
+
+
+                <Grid>
+                                        <Row  style={{  height:200 }} >
+                                          <Image source={require('../public/team.jpg')}   style={{ width: '100%', height: '100%'}}/>
+                                        </Row>
+                                        <Row  style={{  height:300}}>
+                                          <List >
+                                                <ListItem >
+                                                    <Text >Tao</Text>
+                                                </ListItem>
+                                                <ListItem>
+                                                    <Text>P'Nong</Text>
+                                                </ListItem>
+                                                <ListItem>
+                                                    <Text>Nut</Text>
+                                                </ListItem>
+                                                <ListItem>
+                                                    <Text>Toyakung</Text>
+                                                </ListItem>
+                                                <ListItem>
+                                                    <Text>Lit</Text>
+                                                </ListItem>
+                                                <ListItem>
+                                                    <Text>Ball</Text>
+                                                </ListItem>
+
+                                            </List>
+                                          </Row>
+                </Grid>
+
+
+    );
+  }
+}
