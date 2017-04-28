c5c8a884bd6e18e9e4fc38ac1b52556be26cd643
adadesions <ada_dev@icloud.com>
2017-04-28 09:53:44 +0700

    Adjust style

--- SocialNetwork/index.android.js
+++ SocialNetwork/index.android.js
@@ -35,23 +35,4 @@ export default class SocialNetwork extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('SocialNetwork', () => SocialNetwork);
